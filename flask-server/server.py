4fb665d13796faf2f9ccb9a41033ad58669a8486
mFerr16 <mannyferreira12@gmail.com>
2026-02-03 20:50:27 -0500

    connect server to mysql

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,20 +1,47 @@
-from flask import Flask
-from flask_mysqldb import MySQL
+from flask import Flask, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
-"""
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'your_password'
-app.config['MYSQL_DB'] = 'your_database'
+#create connection to db
+con=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='admin@1234', #need to figure out
+    database='sakila'
+    
+)
 
-mysql = MySQL(app)
-"""
+@app.route("/getTable", methods=['GET'])
+def get_tables():
+    cursor=con.cursor()
+    cursor.execute("SHOW TABLES;")
+    tables=cursor.fetchall()
+    cursor.close()
+    table_names=[table[0] for table in tables]
+    return jsonify({"Top Films": table_names}), 200
+
+@app.route("/topFilms", methods=['GET'])
+def topFilms():
+    cursor=con.cursor()
+    cursor.execute("""
+        select f.title
+        from film as f
+        join film_category as fc
+        on f.film_id = fc.film_id
+        join category as c 
+        on fc.category_id = c.category_id
+        limit 5;
+        """)
+    films=cursor.fetchall()
+    cursor.close()
+    film_names = [film[0] for film in films]
+    return jsonify({"Films": film_names}), 200
+
 
 @app.route("/members")
 def members():
     return {"members": ["M1", " M2", "M3"]}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='localhost', port='5000')

acf0e791dac09fd81a1b9be821b0a2b50cce1b13
Jeremiah E <jeremiahe@Jeremiahs-MacBook-Pro.local>
2026-02-07 01:34:43 -0500

    Added top 5 rented films and fixed database credentials and merged with ur changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import NavbarComp from '../components/navbar'
 
 const Home = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/topRentedFilms')
+    .then(res => setData(res.data["Top Rented Films"]))
+    .catch(err => console.log(err))
+  }, [])
+
   return (
     <div>
       <NavbarComp/>
-      <div>Home</div>
+      <div className='container'>
+        <h2 className='mt-3'>Top 5 Rented Films</h2>
+        <table className='table mt-3'>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Times Rented</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.map((film) => {
+                return <tr key={film.film_id}>
+                  <td>{film.title}</td>
+                  <td>{film.rental_count}</td>
+                </tr>
+              })
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }

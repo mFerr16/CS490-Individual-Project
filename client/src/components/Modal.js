d52cc94d5bb6f4bf69249d2ed10bc012005d9834
mFerr16 <mannyferreira12@gmail.com>
2026-02-09 22:52:10 -0500

    added pop up with film click, working on adding dynamic info to pop-up

---
+++ client/src/components/Modal.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+const MODAL_STYLES={
+  position: "fixed",
+  top:"50%",
+  left:"50%",
+  transform:"translate(-50%, -50%)",
+  backgroundColor:"#FFF",
+  padding:"50px",
+  zIndex:1000
+}
+
+const OVERLAY_STYLES={
+  postition:"relative",
+  top:0,
+  left:0,
+  right:0,
+  bottom:0,
+  backgroundColor:"rgba(0, 0, 0, .7)",
+  zIndex:1000
+}
+
+const BUTTON_STYLES={
+  position:"fixed",
+  background:"none",
+  border:"none",
+  top:"10px",
+  right:"10px",
+  height:"5px"
+}
+
+export default function Modal( { open, children, onClose } ) {
+  if(!open) return null 
+  return ReactDOM.createPortal(
+    <>
+      <div style={OVERLAY_STYLES}></div>
+      <div style={MODAL_STYLES}>
+          <button style={BUTTON_STYLES} onClick={onClose}>X</button>
+          {children}
+      </div>
+    </>,
+    document.getElementById("portal")
+  )
+}
--- client/src/pages/Films.jsx
+++ client/src/pages/Films.jsx
@@ -2,11 +2,28 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import NavbarComp from '../components/navbar'
 import DataTable from 'react-data-table-component'
+import { CgInfo } from "react-icons/cg"
+import Modal from '../components/Modal'
+
+const MODHEAD_STYLES={
+  position:"fixed",
+  background:"none",
+  border:"none",
+  top:0,
+  left:"10px",
+  height:"5px"
+}
 
 const Films = () => {
-const [data, setData] = useState([])
-const [records, setRecords] = useState([]);
 
+  const [data, setData] = useState([])
+  const [records, setRecords] = useState([]);
+  const [modInfo, setModInfo] = useState([]);
+
+  const [show, setShow] = useState(false);
+  const [modTitle, setModTitle] = useState("");
+  
+          
   useEffect(()=>{
     axios.get('http://localhost:5000/getFilms')
     .then(res=> {
@@ -15,21 +32,38 @@ const [records, setRecords] = useState([]);
     })
     .catch(err=>console.log(err))
   }, [])
+  
+  const openMod = async(row)=>{ 
+    await axios.get('http://localhost:5000/getFilmInfo/'+JSON.stringify(row.film_id)).then(res=> {
+      setModInfo(res.data.Info);
+      setModTitle(row.title);
+      console.log(modInfo)
+    }).then(handleShow).catch(err=>console.log(err))
+  }
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
 
   const columns = [
     {
-      name: 'Film ID',
+      name: "",
+      sortable: false,
+      button: "true",
       cell: row=>(
-        <div data-tag="allowRowEvents" onClick={e => handleRowClicked(e, row.film_id)} style={{ cursor: 'pointer' }}>
-          {row.film_id}
-        </div>
+        <button style={{background:'none', border:'none'}} onClick={()=>openMod(row)}><CgInfo/></button>
       ),
+      allowOverFlow: true
+    },
+    {
+      name: 'Film ID',
+      selector: row=>row.film_id,
       sortable: true
     },
     {
       name: 'Title',
       selector: row=>row.title,
-      sortable: true
+      sortable: true,
     },
     {
       name: 'Category',
@@ -39,7 +73,8 @@ const [records, setRecords] = useState([]);
     {
       name: 'Last Update',
       selector: row=>row.last_update
-    },
+    }
+    
   ]
 
   function handleFilter(event){
@@ -53,31 +88,28 @@ const [records, setRecords] = useState([]);
         setRecords(newData)
   }
 
-  const [openPopup, setOpenPopup] = useState(false)
-
-  // not working
-  const handleRowClicked = (row, event) => {
-      console.log(row.film_id)
-      return(
-      <div className="rounded-md p-4 bg-white popup">
-        <div className = "flex flex-row justify-between">
-          <h2>Popup</h2>
-          <button onClick={()=>setOpenPopup(false)}></button>
-        </div>
-        <p className ="text-xl">Popup message here: </p>
-      </div>
-      )
-  }
-
   return (
-    <div>
-      <NavbarComp/>
+    <>
+      <div>
+        <NavbarComp/>
 
-      <div className='container mt-5'>
-        <input placeholder="Search" type="text" onChange={handleFilter}/>
-        <DataTable columns={columns} data={records} fixedHeader pagination theme='light' onRowClicked={handleRowClicked} />
+        <div className='container mt-5'>
+          <input placeholder="Search" type="text" onChange={handleFilter}/>
+          <DataTable columns={columns} data={records} fixedHeader pagination theme='light' />
+        </div>
+        <Modal open={show} onClose={handleClose}>
+            <div style={MODHEAD_STYLES}>
+              <p>{modTitle}</p>
+            </div>
+            <div>
+              Description: {modInfo.description} <br/>
+              Rating: {modInfo.rating} <br/>
+              Rental Duration: {modInfo.rental_duration} <br/>
+              Rental Rate: {modInfo.rental_rate} <br/>
+            </div>
+        </Modal>
       </div>
-    </div>
+    </>
   )
 }
 
